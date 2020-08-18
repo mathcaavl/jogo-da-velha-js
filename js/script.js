abbb85b4b42c56ceac5e3ac3bb0472d647aa2807
mathcaavl <mateuscavl1@gmail.com>
2020-08-18 17:56:14 -0300

    Jogo da Velha Finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // ...
 const listBtn = []
+const restart = document.querySelector('.btnRestart');
 
 // Definindo Variaveis
 listBtn.push(document.querySelector('.casa1'));
@@ -12,10 +13,19 @@ listBtn.push(document.querySelector('.casa7'));
 listBtn.push(document.querySelector('.casa8'));
 listBtn.push(document.querySelector('.casa9'));
 
+// Criando Matriz
+const matrizTabuleiro = [
+    [document.querySelector('.casa1'), document.querySelector('.casa2'), document.querySelector('.casa3')],
+    [document.querySelector('.casa4'), document.querySelector('.casa5'), document.querySelector('.casa6')],
+    [document.querySelector('.casa7'), document.querySelector('.casa8'), document.querySelector('.casa9')],
+]
+
 // Eventos
 for (let i = 0; i < listBtn.length; i++) {
     listBtn[i].addEventListener('click', preencherValor);
 }
+
+restart.addEventListener('click', reiniciar);
 
 // Objeto para definir a vez 
 var turno = {
@@ -32,15 +42,82 @@ var turno = {
 
 // Funções
 function preencherValor (e) {
-    const imagem = document.createElement('img');
-
     if (turno.atual == 0) {
-        imagem.src="images/x.jpg";
-        e.target.appendChild(imagem)
+        e.target.value = "X"
+        e.target.innerHTML = e.target.value;
+        verificarVencedor();
         turno.set(1);
     } else { 
-        imagem.src="images/bola.jpg"
-        e.target.appendChild(imagem);
+        e.target.value = "O"
+        e.target.innerHTML = e.target.value;
+        verificarVencedor();
         turno.set(0);
     }
 }
+
+function verificarVencedor () {
+    if (listBtn[0].value == 'X' && listBtn[1].value == 'X' && listBtn[2].value == 'X') {
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[3].value == 'X' && listBtn[4].value == 'X' && listBtn[5].value == 'X') {
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[6].value == 'X' && listBtn[7].value == 'X' && listBtn[8].value == 'X') {
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[0].value == 'X' && listBtn[3].value == 'X' && listBtn[6].value == 'X') {
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[1].value == 'X' && listBtn[4].value == 'X' && listBtn[7].value == 'X') {
+        alert('X Venceu');
+        reiniciar();
+    } else if (listBtn[2].value == 'X' && listBtn[5].value == 'X' && listBtn[8].value == 'X') {
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[0].value == 'X' && listBtn[4].value == 'X' && listBtn[8].value == 'X') { 
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[2].value == 'X' && listBtn[4].value == 'X' && listBtn[6].value == 'X') { //
+        alert('X Venceu')
+        reiniciar();
+    } else if (listBtn[0].value == 'O' && listBtn[1].value == 'O' && listBtn[2].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[3].value == 'O' && listBtn[4].value == 'O' && listBtn[5].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[6].value == 'O' && listBtn[7].value == 'O' && listBtn[8].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[0].value == 'O' && listBtn[3].value == 'O' && listBtn[6].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[1].value == 'O' && listBtn[4].value == 'O' && listBtn[7].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[2].value == 'O' && listBtn[5].value == 'O' && listBtn[8].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[0].value == 'O' && listBtn[4].value == 'O' && listBtn[8].value == 'O') {
+        alert('O Venceu');
+        reiniciar();
+    } else if (listBtn[2].value == 'O' && listBtn[4].value == 'O' && listBtn[6].value == 'O') {
+        alert('O Venceu')
+        reiniciar();
+    } else if (listBtn[0].value != "" && listBtn[1].value != "" && listBtn[2].value != "" && listBtn[3].value != "" && listBtn[4].value != "" && listBtn[5].value != "" && listBtn[6].value != "" && listBtn[7].value != "" && listBtn[8].value != "") {
+        alert('Deu Velha!')
+        reiniciar();
+    }
+}
+
+function reiniciar () {
+    for (let i = 0; i < listBtn.length; i++) {
+        listBtn[i].innerHTML = "";
+    }
+}
+
+//combinações vencedoras:
+//123, 456, 789
+//146, 258, 369
+//159, 357 
+//listBtn[i].getAttribute('img')
